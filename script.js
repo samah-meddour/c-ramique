b84d285d489ad4a0b21d1fff1d12181392747391
samah-meddour <zk.samah.meddour@outlook.com>
2024-07-30 10:11:02 +0200

    version final avec java

---
+++ script.js
@@ -0,0 +1,25 @@
+let prenom = prompt("Quel est ton prenom ?");
+let nom = prompt("Quel est ton nom ?");
+let welcomTitle = document.getElementById("welcomTitle")
+welcomTitle.innerText = "Initiez vous à la céramique " + prenom;
+let isOk = prompt("Avez-vous déjà fait de la céramique ? répondre par oui ou par non");
+
+if ((isOk == "oui") || (isOk == "OUI") || (isOk == "Oui"))
+{
+	//fair un truc
+	console.log("l'utilisateur a fait de la céramique")
+}
+else if ((isOk == "non") || (isOk"NON") || (isOk"Non")
+{
+	//faire autre chose
+	console.log("l'utilisateur n'a pas fait de céramique")
+}
+else
+{
+	//faire un truc dans le cas ou l'utilisateur fais ce qu'il veut
+	console.log("l'utilisateur ne répond pas à la question")
+}
+/*console.log("Bonjour " + prenom + "" + nom);
+console.log("Comment vas-tu " + prenom + " ?"); 
+
+alert("Salut " + nom + prenom + " !" )*/
